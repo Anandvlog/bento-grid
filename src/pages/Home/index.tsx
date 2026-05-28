80d67734f7145a8f3c038efc9da09a2253e0c2d9
Neha chauhan <anandvlog03@gmail.com>
2026-05-28 15:02:33 +0530

    add minnor changes

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -9,7 +9,7 @@ const Homepage = () => {
           Create and schedule content <span>quicker.</span>
         </h2>
         <img
-          src="/src/assets/images/illustration-create-post.webp"
+          src="../public/assets/images/illustration-create-post.webp"
           alt="Create Content"
         />
       </div>
@@ -20,7 +20,7 @@ const Homepage = () => {
         </h2>
         <img
           alt="five-star"
-          src="/src/assets/images/illustration-five-stars.webp"
+          src="../public/assets/images/illustration-five-stars.webp"
         />
         <p>Over 4,000 5-star reviews</p>
       </div>
@@ -28,7 +28,7 @@ const Homepage = () => {
       <div className="schedule_card">
         <h3>Schedule to social media.</h3>
         <img
-          src="/src/assets/images/illustration-schedule-posts.webp"
+          src="../public/assets/images/illustration-schedule-posts.webp"
           alt="Schedule Content"
         />
         <p>
@@ -40,29 +40,29 @@ const Homepage = () => {
       <div className="ai-card">
         <h2>Write your content using AI.</h2>
         <img
-          src="/src/assets/images/illustration-ai-content.webp"
+          src="../public/assets/images/illustration-ai-content.webp"
           alt="AI Content"
         />
       </div>
 
       <div className="multi-media-card">
-        <img alt="social-media" src="/src/assets/images/illustration-multiple-platforms.webp"  />
+        <img alt="social-media" src="../public/assets/images/illustration-multiple-platforms.webp"  />
         <h2>Manage multiple accounts and platforms.</h2>
       </div>
 
       <div className="maintain-card">
           <h2>Maintain a consistent posting schedule.</h2>
-          <img src="/src/assets/images/illustration-consistent-schedule.webp" alt="Maintain Schedule" />
+          <img src="../public/assets/images/illustration-consistent-schedule.webp" alt="Maintain Schedule" />
       </div>
 
         <div className="audience-card">
           <h2>{">"}56%</h2>
           <p>faster audience growth</p>
-          <img alt="audience growth" src="/src/assets/images/illustration-audience-growth.webp" />
+          <img alt="audience growth" src="../public/assets/images/illustration-audience-growth.webp" />
         </div>
 
         <div className="followers-card">
-              <img alt="followers" src="/src/assets/images/illustration-grow-followers.webp" />
+              <img alt="followers" src="../public/assets/images/illustration-grow-followers.webp" />
               <h2>Grow followers with non-stop content.</h2>
         </div>
    </div>
