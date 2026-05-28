bc094b35b23d7a8799de3532da305b197d49ab09
Neha chauhan <anandvlog03@gmail.com>
2026-05-28 11:19:36 +0530

    add design style css code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import './App.css'
+import Homepage from './pages/Home'
 
 function App() {
 
   return (
     <>
-      <h1>Welcome to React with TypeScript!</h1>
+    <Homepage />
     </>
   )
 }
